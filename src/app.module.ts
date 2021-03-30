7ae6b5118531c33ecc8467ee3e1ce0336509abfd
Sina Montazeri <sinamontazeri@outlook.com>
2021-03-30 23:50:15 +0430

    add notes module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { NotesModule } from './notes/notes.module';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
 
 @Module({
-  imports: [],
+  imports: [NotesModule],
+  controllers: [TasksController],
+  providers: [TasksService],
 })
 export class AppModule {}
