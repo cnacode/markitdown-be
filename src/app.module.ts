617da848e7728516bfae9ff721f3fa7ce4a13af4
Sina Montazeri <sinamontazeri@outlook.com>
2021-05-10 18:06:30 +0430

    add back logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { NotesModule } from './notes/notes.module';
+import { MetaModule } from './meta/meta.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, NotesModule, MetaModule],
   controllers: [AppController],
   providers: [AppService],
 })
